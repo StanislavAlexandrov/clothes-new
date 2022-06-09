0491810481d9d4180e84b5150e1b92ee64ea0b0e
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2022-06-10 01:08:54 +1100

    added more articles of clothing

--- src/data/data.js
+++ src/data/data.js
@@ -1,71 +1,228 @@
-import img1 from '../images/IMAGE 2022-06-08 12:52:05.jpg';
-import img2 from '../images/IMAGE 2022-06-08 12:52:21.jpg';
-import img3 from '../images/IMAGE 2022-06-08 12:52:26.jpg';
-import img4 from '../images/IMAGE 2022-06-08 12:52:28.jpg';
-import img5 from '../images/IMAGE 2022-06-08 12:52:33.jpg';
-import img6 from '../images/IMAGE 2022-06-08 12:52:48.jpg';
-import img7 from '../images/IMAGE 2022-06-08 12:52:51.jpg';
-import img8 from '../images/IMAGE 2022-06-08 12:52:55.jpg';
-import img9 from '../images/IMAGE 2022-06-08 12:53:15.jpg';
-import img10 from '../images/IMAGE 2022-06-08 12:53:26.jpg';
-import img11 from '../images/IMAGE 2022-06-08 12:53:28.jpg';
-import img12 from '../images/IMAGE 2022-06-08 12:53:31.jpg';
-import img13 from '../images/IMAGE 2022-06-08 12:53:33.jpg';
-import img14 from '../images/IMAGE 2022-06-08 12:53:35.jpg';
-import img15 from '../images/IMAGE 2022-06-08 12:53:39.jpg';
-import img16 from '../images/IMAGE 2022-06-08 12:53:41.jpg';
-import img17 from '../images/IMAGE 2022-06-08 12:53:43.jpg';
-import img19 from '../images/IMAGE 2022-06-08 12:53:45.jpg';
-import img20 from '../images/IMAGE 2022-06-08 12:53:47.jpg';
-import img21 from '../images/IMAGE 2022-06-08 12:53:49.jpg';
-import img22 from '../images/IMAGE 2022-06-08 12:53:52.jpg';
-import img23 from '../images/IMAGE 2022-06-08 12:53:55.jpg';
-import img24 from '../images/IMAGE 2022-06-08 12:53:57.jpg';
-import img25 from '../images/IMAGE 2022-06-08 12:53:59.jpg';
+import abercrombieBodyM1000 from '../images/abercrombie-body-m-1000.jpg';
+import abercrombieDressL3500 from '../images/abercrombie-dress-l-3500.jpg';
+import armaniJeans27r7900 from '../images/armani-jeans-27r-7900.jpg';
+import armaniPantsM3900 from '../images/armani-pants-m-3900.jpg';
+import bananaDress45800 from '../images/banana-4-dress-5800.jpg';
+import bananaBlouseS4500 from '../images/banana-blouse-s-4500.jpg';
+import bananaBlouseXS2100 from '../images/banana-blouse-xs-2100.jpg';
+import bananaCardiganS4900 from '../images/banana-cardigan-s-4900.jpg';
+import bananaDress67500 from '../images/banana-dress-6-7500.jpg';
+import bananaJacketSkirt0279002900 from '../images/banana-jacket-skirt-02-7900-2900.jpg';
+import bananaJeans295900 from '../images/banana-jeans-29-5900.jpg';
+import bananaSweaterS3900 from '../images/banana-sweater-s-3900.jpg';
+import bananaTeeS1500 from '../images/banana-tee-s-1500.jpg';
+import bananaTopLongSleeveS1900 from '../images/banana-top-long-sleeve-s-1900.jpg';
+import bananaTopS1900 from '../images/banana-top-s-1900.jpg';
+import bananaTopS2900 from '../images/banana-top-s-2900.jpg';
+import calvinLeggings78M4000 from '../images/calvin-leggings-78-m-4000.jpg';
+import calvinTeeS1000 from '../images/calvin-tee-s-1000.jpg';
+import coatM16900_1 from '../images/coat-m-16900-1.jpg';
+import coatM16900_2 from '../images/coat-m-16900-2.jpg';
+import gapM1900Floral from '../images/gap-m-1900-floral.jpg';
+import gapM1900 from '../images/gap-m-1900.jpg';
+import gapShirtXS1000 from '../images/gap-shirt-xs-1000.jpg';
+import gapShorts62900 from '../images/gap-shorts-6-2900.jpg';
+import jCrew65900 from '../images/jcrew-6-5900.jpg';
+import jCrewBlouseM1900 from '../images/jcrew-blouse-m-1900.jpg';
+import jCrewBlouseS1700 from '../images/jcrew-blouse-s-1700.jpg';
+import jCrewDress62500 from '../images/jcrew-dress-6-2500.jpg';
+import jCrewJacket67500 from '../images/jcrew-jacket-6-7500.jpg';
+import jCrewSweaterM2900 from '../images/jcrew-sweater-m-2900.jpg';
+import jCrewSweaterM4500 from '../images/jcrew-sweater-m-4500.jpg';
+import jCrewTurtleneckM1000 from '../images/jcrew-turtleneck-m-1000.jpg';
+import mKDressS6900 from '../images/mk-dress-s-6900.jpg';
+import tedBakerMLDress13500 from '../images/tedbaker-ml-dress-13500.jpg';
+import tedBakerTopM5000 from '../images/tedbaker-top-m-5000.jpg';
+import tommyDress64900 from '../images/tommy-dress-6-4900.jpg';
+import twinSetTeeM4500 from '../images/twinset-tee-m-4500.jpg';
+import whbmDress6900 from '../images/whbm-dress-6900.jpg';
 
 const DATA = [
     {
         name: 'Платье J CREW',
         size: '6(M)',
         price: 5900,
-        image: img1,
+        image: jCrew65900,
         description: 'новое с этикетками',
     },
-    { name: 'Платье GAP', size: 'M', price: 1900, image: img2 },
+    { name: 'Платье GAP', size: 'M', price: 1900, image: gapM1900Floral },
+    { name: 'Платье GAP', size: 'M', price: 1900, image: gapM1900 },
     {
         name: 'Двустороннее платье White House Black Market',
         size: 8,
         price: 6900,
-        image: img3,
+        image: whbmDress6900,
     },
-    { name: 'Плащ с кружевом,', size: 'M', price: 16900, image: img4 },
-    { name: 'Платье J Crew', size: '6', price: 2500, image: img5 },
-    { name: 'Twinset Milano футболка', size: 'M', price: 4500, image: img6 },
+    { name: 'Плащ с кружевом,', size: 'M', price: 16900, image: coatM16900_1 },
+    { name: 'Платье J Crew', size: '6', price: 2500, image: jCrewDress62500 },
+    {
+        name: 'Twinset Milano футболка',
+        size: 'M',
+        price: 4500,
+        image: twinSetTeeM4500,
+    },
     {
         name: 'Джинсы Banana Republic (высокая посадка, до щиколоток, прямые)',
         size: 29,
         price: 5900,
-        image: img7,
+        image: bananaJeans295900,
     },
-    { name: 'Шорты Gap', size: 6, price: 2900, image: img8 },
-    { name: 'Блузка J CREW', size: 'S', price: 1700, image: img9 },
-    { name: 'Блузка J CREW', size: 'M', price: 1900, image: img10 },
-    { name: 'Свитер J CREW', size: 'M', price: 4500, image: img11 },
-    { name: 'Платье TED BAKER', size: 'M-L', price: 13500, image: img12 },
-    { name: 'Calvin Klein леггинсы 7/8', size: 'M', price: 4000, image: img13 },
+    { name: 'Шорты Gap', size: 6, price: 2900, image: gapShorts62900 },
+    { name: 'Блузка J CREW', size: 'S', price: 1700, image: jCrewBlouseS1700 },
+    { name: 'Блузка J CREW', size: 'M', price: 1900, image: jCrewBlouseM1900 },
+    { name: 'Свитер J CREW', size: 'M', price: 4500, image: jCrewSweaterM4500 },
+    {
+        name: 'Платье TED BAKER',
+        size: 'M-L',
+        price: 13500,
+        image: tedBakerMLDress13500,
+    },
+    {
+        name: ' Платье Banana Republic',
+        size: '6',
+        price: 7500,
+        image: bananaDress67500,
+    },
+
+    {
+        name: 'Calvin Klein леггинсы 7/8',
+        size: 'M',
+        price: 4000,
+        image: calvinLeggings78M4000,
+    },
     {
         name: 'Джинсы чёрные, тонкие, высокая посадка ARMANI EXCHANGE',
         size: '27R',
         price: 7900,
-        image: img14,
+        image: armaniJeans27r7900,
     },
-    { name: 'Пиджак с поясом J CREW', size: '6', price: 7500, image: img15 },
-    { name: 'Платье Abercrombie&Fitch', size: 'L', price: 3500, image: img16 },
+    {
+        name: 'Пиджак с поясом J CREW',
+        size: '6',
+        price: 7500,
+        image: jCrewJacket67500,
+    },
+    {
+        name: 'Платье Abercrombie&Fitch',
+        size: 'L',
+        price: 3500,
+        image: abercrombieDressL3500,
+    },
     {
         name: 'Платье с поясом Michael Kors',
         size: 'S',
         price: 6900,
-        image: img17,
+        image: mKDressS6900,
+    },
+    {
+        name: 'Платье Banana Republic',
+        size: '4',
+        price: 5800,
+        image: bananaDress45800,
+    },
+    {
+        name: 'Кардиган Banana Republic',
+        size: 'S',
+        price: 4900,
+        image: bananaCardiganS4900,
+    },
+    {
+        name: 'Пиджак (0) и юбка (2) Banana Republic',
+        size: '0+2',
+        price: '7900/2900',
+        image: bananaJacketSkirt0279002900,
+    },
+    {
+        name: 'Блуза Banana Republic',
+        size: 'S',
+        price: 4500,
+        image: bananaBlouseS4500,
+    },
+    {
+        name: 'Блузка Banana Republic',
+        size: 'XS',
+        price: 2100,
+        image: bananaBlouseXS2100,
+    },
+    {
+        name: 'Топ Banana Republic',
+        size: 'S',
+        price: 1900,
+        image: bananaTopS1900,
+    },
+    {
+        name: 'Платье Tommy Hilfiger',
+        size: '6',
+        price: 4900,
+        image: tommyDress64900,
+        description: 'Новое без этикетки',
+    },
+    {
+        name: 'Топ Banana Republic',
+        size: 'S',
+        price: 2900,
+        image: bananaTopS2900,
+    },
+    {
+        name: 'Топ TED BAKER',
+        size: 'M',
+        price: 5000,
+        image: tedBakerTopM5000,
+    },
+    {
+        name: 'Свитер J.CREW',
+        size: 'M',
+        price: 2900,
+        image: jCrewSweaterM2900,
+    },
+    {
+        name: 'Водолазка J. CREW,',
+        size: 'M',
+        price: 1000,
+        image: jCrewTurtleneckM1000,
+    },
+    {
+        name: 'Топ с длинными рукавами Banana Republic',
+        size: 'S',
+        price: 1900,
+        image: bananaTopLongSleeveS1900,
+    },
+    {
+        name: 'Свитер Banana Republic',
+        size: 'S',
+        price: 3900,
+        image: bananaSweaterS3900,
+    },
+    {
+        name: 'Майка Banana Republic',
+        size: 'S',
+        price: 1500,
+        image: bananaTeeS1500,
+    },
+    {
+        name: 'Рубашка GAP',
+        size: 'XS',
+        price: 1000,
+        image: gapShirtXS1000,
+    },
+    {
+        name: 'Боди Abercrombie&Fitch',
+        size: 'M',
+        price: 1000,
+        image: abercrombieBodyM1000,
+    },
+    {
+        name: 'Брюки спортивные Armani Exchange',
+        size: 'M',
+        price: 3900,
+        image: armaniPantsM3900,
+        description: 'Новые без этикетки',
+    },
+    {
+        name: 'Calvin Klein Jeans футболка',
+        size: 'S',
+        price: 1000,
+        image: calvinTeeS1000,
     },
 ];
 
